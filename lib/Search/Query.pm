2a259d5601711225aa866a5e8e8782b6688bacd9
Peter Karman <perl@peknet.com>
2010-03-04 04:57:28 +0000

    Import of KARMAN/Search-Query-0.09 from CPAN.
    gitpan-cpan-distribution: Search-Query
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         KARMAN/Search-Query-0.09.tar.gz
    gitpan-cpan-author:       KARMAN
    gitpan-cpan-maturity:     released

--- lib/Search/Query.pm
+++ lib/Search/Query.pm
@@ -11,7 +11,7 @@ use Module::Pluggable
     search_path => ['Search::Query::Dialect'],
     sub_name    => 'dialects';
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 =head1 NAME
 
@@ -29,9 +29,9 @@ Search::Query - polyglot query parsing
 
 =head1 DESCRIPTION
 
-This class provides documentation and a single class method.
+This class provides documentation and class methods.
 
-This module started as a fork of the excellent Search::QueryParser module
+Search::Query started as a fork of the excellent Search::QueryParser module
 and was then rewritten to provide support for alternate query dialects.
 
 =head1 METHODS
